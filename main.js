9b2983cdd75a83b4f76172ca62cf788e1e8e72ae
Arnaud-Neto <arnaudmacedomelo@gmail.com>
2024-05-22 15:21:22 -0300

    check para ver se o navegador tem geolocation no main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+
+if ("geolocation" in navigator){
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+} else {
+    alert(
+        "Me desculpe mas o seu navegador não suporta geolocation"
+    )
+    
+    document.write("Não disponivel")
+}
